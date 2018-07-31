4f8d9c3a6993cbcbc532f55f32fd61ceb2c4cefa
ggw <ggy@6shangwang.com>
2018-07-31 16:59:17 +0800

    用户列表

--- src/main/java/com/mmall/service/SysDeptService.java
+++ src/main/java/com/mmall/service/SysDeptService.java
@@ -1,11 +1,13 @@
 package com.mmall.service;
 
 import com.google.common.base.Preconditions;
+import com.mmall.common.RequestHolder;
 import com.mmall.dao.SysDeptMapper;
 import com.mmall.exception.ParamException;
 import com.mmall.model.SysDept;
 import com.mmall.param.DeptParam;
 import com.mmall.util.BeanValidator;
+import com.mmall.util.IpUtil;
 import com.mmall.util.LevelUtil;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
@@ -29,8 +31,8 @@ public class SysDeptService {
         SysDept dept = SysDept.builder().name(param.getName()).parentId(param.getParentId())
                 .seq(param.getSeq()).remark(param.getRemark()).build();
         dept.setLevel(LevelUtil.calculateLevel(getLevel(param.getParentId()), param.getParentId()));
-        dept.setOperator("system");//TODO
-        dept.setOperateIp("127.0.0.1");//TODO
+        dept.setOperator(RequestHolder.getCurrentUser().getUsername());
+        dept.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
         dept.setOperateTime(new Date());
         sysDeptMapper.insertSelective(dept);
     }
@@ -48,8 +50,8 @@ public class SysDeptService {
         SysDept after=SysDept.builder().id(param.getId()).name(param.getName()).parentId(param.getParentId())
                 .seq(param.getSeq()).remark(param.getRemark()).build();
         after.setLevel(LevelUtil.calculateLevel(getLevel(param.getParentId()),param.getParentId()));
-        after.setOperator("system-update");//TODO
-        after.setOperateIp("127.0.0.1");//TODO
+        after.setOperator(RequestHolder.getCurrentUser().getUsername());
+        after.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
         after.setOperateTime(new Date());
 
         updateWithChild(before,after);
--- src/main/java/com/mmall/service/SysUserService.java
+++ src/main/java/com/mmall/service/SysUserService.java
@@ -1,17 +1,22 @@
 package com.mmall.service;
 
 import com.google.common.base.Preconditions;
+import com.mmall.beans.PageQuery;
+import com.mmall.beans.PageResult;
+import com.mmall.common.RequestHolder;
 import com.mmall.dao.SysUserMapper;
 import com.mmall.exception.ParamException;
 import com.mmall.model.SysUser;
 import com.mmall.param.UserParam;
 import com.mmall.util.BeanValidator;
+import com.mmall.util.IpUtil;
 import com.mmall.util.MD5Util;
 import com.mmall.util.PasswordUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class SysUserService {
@@ -19,23 +24,23 @@ public class SysUserService {
     @Resource
     private SysUserMapper sysUserMapper;
 
-    public void save(UserParam param){
+    public void save(UserParam param) {
         BeanValidator.check(param);
-        if(checkEmailExist(param.getMail(),param.getId())){
+        if (checkEmailExist(param.getMail(), param.getId())) {
             throw new ParamException("邮箱已被占用");
         }
-        if(checkTelephoneExist(param.getTelephone(),param.getId())){
+        if (checkTelephoneExist(param.getTelephone(), param.getId())) {
             throw new ParamException("电话已被占用");
         }
-        String password= PasswordUtil.randomPassword();
+        String password = PasswordUtil.randomPassword();
         //TODO
-        password="12345678";
-        String encryptedPassword= MD5Util.encrypt(password);
-        SysUser user=SysUser.builder().username(param.getUsername()).telephone(param.getTelephone())
+        password = "12345678";
+        String encryptedPassword = MD5Util.encrypt(password);
+        SysUser user = SysUser.builder().username(param.getUsername()).telephone(param.getTelephone())
                 .mail(param.getMail()).password(encryptedPassword).deptId(param.getDeptId()).status(param.getStatus())
                 .remark(param.getRemark()).build();
-        user.setOperator("system");//TODO
-        user.setOperateIp("127.0.0.1");//TODO
+        user.setOperator(RequestHolder.getCurrentUser().getUsername());
+        user.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
         user.setOperateTime(new Date());
 
         //TODO:sendEmail
@@ -43,31 +48,44 @@ public class SysUserService {
         sysUserMapper.insertSelective(user);
     }
 
-    public boolean checkTelephoneExist(String telephone, Integer id){
-        return sysUserMapper.countByTelephone(telephone,id)>0;
+    public boolean checkTelephoneExist(String telephone, Integer id) {
+        return sysUserMapper.countByTelephone(telephone, id) > 0;
     }
 
-    public boolean checkEmailExist(String mail,Integer id){
-        return sysUserMapper.countByMail(mail,id)>0;
+    public boolean checkEmailExist(String mail, Integer id) {
+        return sysUserMapper.countByMail(mail, id) > 0;
     }
 
-    public void update(UserParam param){
+    public void update(UserParam param) {
         BeanValidator.check(param);
-        if(checkEmailExist(param.getMail(),param.getId())){
+        if (checkEmailExist(param.getMail(), param.getId())) {
             throw new ParamException("邮箱已被占用");
         }
-        if(checkTelephoneExist(param.getTelephone(),param.getId())){
+        if (checkTelephoneExist(param.getTelephone(), param.getId())) {
             throw new ParamException("电话已被占用");
         }
-        SysUser before=sysUserMapper.selectByPrimaryKey(param.getId());
-        Preconditions.checkNotNull(before,"待更新的用户不存在");
-        SysUser after=SysUser.builder().id(param.getId()).username(param.getUsername()).telephone(param.getTelephone())
+        SysUser before = sysUserMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(before, "待更新的用户不存在");
+        SysUser after = SysUser.builder().id(param.getId()).username(param.getUsername()).telephone(param.getTelephone())
                 .mail(param.getMail()).deptId(param.getDeptId()).status(param.getStatus())
                 .remark(param.getRemark()).build();
+        after.setOperator(RequestHolder.getCurrentUser().getUsername());
+        after.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
+        after.setOperateTime(new Date());
         sysUserMapper.updateByPrimaryKeySelective(after);
     }
 
     public SysUser findByKeyword(String keyword) {
         return sysUserMapper.findByKeyword(keyword);
+    }
+
+    public PageResult<SysUser> getPageByDeptId(int deptId, PageQuery page) {
+        BeanValidator.check(page);
+        int count = sysUserMapper.countByDeptId(deptId);
+        if (count > 0) {
+            List<SysUser> list = sysUserMapper.getPageByDeptId(deptId, page);
+            return PageResult.<SysUser>builder().total(count).data(list).build();
+        }
+        return PageResult.<SysUser>builder().build();
     }
 }
